079f89683cd99761eb97806870b04fe81a1b3489
Sekhar Ravinutala <sekhar@allurefx.com>
2019-01-28 15:40:59 -0800

    Initial commit

---
+++ run.py
@@ -0,0 +1,21 @@
+"""
+Car sales.
+"""
+
+
+class Run(object):
+    """
+    Run the sales.
+    """
+
+    def sales(self):
+        """
+        Simulate sales using test data and print out customer and agent reports showing
+            (1) Summary statistics, with the mean, median, and SD of customer wait times.
+            (2) Agent data showing agent ID, deals closed, total revenue generated,
+                commission earned, and bonus awarded, in a tabular form.
+        """
+
+
+if __name__ == "__main__":
+    Run().sales()
